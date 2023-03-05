defdc25f499ce9fdc89f34e6b0308b3f40a254bc
davidmuir1999 <david.bme.muir2017@gmail.com>
2023-03-05 16:09:09 +0000

    Home Page Routing and Header Styling

---
+++ src/Pages/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Home.css';
+import { Link } from "react-router-dom";
+import AppsIcon from '@mui/icons-material/Apps';
+import { Avatar } from "@mui/material";
+
+function Home() {
+    return(
+        <div className="home">
+            <div className="home__header">
+                <div className="home__header--Left">
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
+                <div className="home__header--Right">
+                <Link to='/gmail'>Gmail</Link>
+                <Link to='/images'>Images</Link>
+                <AppsIcon/>
+                <Avatar/>
+                </div>
+            </div>
+            <div className="home__body">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home;
