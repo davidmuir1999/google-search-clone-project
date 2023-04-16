e9a941b0d90bb9acf24a7060b873010788f99401
davidmuir1999 <david.bme.muir2017@gmail.com>
2023-04-16 21:30:10 +0100

    implement firebase

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASXMf6sGUeZXADlo-r_1SgR9s6sNLdvNY",
+  authDomain: "clone-b1c83.firebaseapp.com",
+  projectId: "clone-b1c83",
+  storageBucket: "clone-b1c83.appspot.com",
+  messagingSenderId: "692665325692",
+  appId: "1:692665325692:web:d62969450647a5e714d1f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
